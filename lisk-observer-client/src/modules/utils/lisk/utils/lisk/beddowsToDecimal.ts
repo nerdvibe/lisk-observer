02c22b639cbc36e1049f5973d21a4ef52da5a413
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2022-01-02 19:52:55 +0100

    Added big amount display method

--- lisk-observer-client/src/modules/utils/lisk/utils/lisk/beddowsToDecimal.ts
+++ lisk-observer-client/src/modules/utils/lisk/utils/lisk/beddowsToDecimal.ts
@@ -3,3 +3,17 @@ import { Big, BigSource } from "big.js";
 export const beddowsToDecimal = (amount: BigSource, round: number = 8) => {
   return Big(amount).div(100000000).round(round);
 };
+
+export const displayBigNumber = (amount: string | number) => {
+  const expAmount = Big(amount).div(100000000);
+  const expNumbers = expAmount.e;
+  const fixedValueResult = Big(expAmount)
+    .toFixed(expNumbers < 0 ? expNumbers * -1 : expNumbers)
+    .toString();
+  const hasDecimal = +expAmount.toString().split(".")[1] > 0;
+  return hasDecimal
+    ? `${(+fixedValueResult.split(".")[0]).toLocaleString()}.${
+        expAmount.toString().split(".")[1]
+      }`
+    : fixedValueResult;
+};
