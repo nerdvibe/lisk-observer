5ed38cf5afe83cc5191afdad76dfd345127d9bc0
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2022-01-19 23:55:17 +0100

    Added cookie banner logic

--- lisk-observer-client/src/UI/cookieBanner/CookieBanner.tsx
+++ lisk-observer-client/src/UI/cookieBanner/CookieBanner.tsx
@@ -1,60 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { NoData } from "../../modules/account/accountProfile/noTransactions/NoData";
 import "./style.css";
 
 export const CookieBanner = () => {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        zIndex: 999999,
-        bottom: "10em",
-        right: "2em",
-        display: "flex",
-      }}
-    >
-      <div
-        className="thought"
-        style={{
-          right: "12px",
-          bottom: "25px",
-        }}
-      >
-        <div
-          className="col12"
-          style={{
-            padding: "15px",
-          }}
-        >
-          <div className="row">
-            <p
-              style={{
-                color: "black",
-                textAlign: "left",
-              }}
-            >
+  const [className, setClassName] = useState("");
+  const [acceptedCookies] = useState(
+    !!localStorage.getItem("COOKIES_ACCEPTED") || false
+  );
+
+  const acceptCookies = () => {
+    localStorage.setItem("COOKIES_ACCEPTED", `${new Date()}`);
+    setClassName("fade-out");
+  };
+
+  return !acceptedCookies ? (
+    <div className={`cookie-banner-container ${className}`}>
+      <div className="thought">
+        <div className="px-4 py-2">
+          <div>
+            <p className="text-black">
               Hey there! I just wanted to let you know that Lisk Observer uses
               only essential functional cookies to work :)
             </p>
           </div>
-          <div
-            className="row"
-            style={{
-              marginTssop: "15px",
-            }}
-          >
-            <a href="#">Sure!</a>
+          <div className="mt-3 purple-text" onClick={acceptCookies}>
+            Sure!
           </div>
         </div>
       </div>
-      <div
-        style={{
-          alignSelf: "flex-end",
-          marginBottom: "-67px",
-        }}
-      >
+      <div className="cookie-robot">
         <NoData message={"."} />
       </div>
     </div>
+  ) : (
+    <></>
   );
 };
