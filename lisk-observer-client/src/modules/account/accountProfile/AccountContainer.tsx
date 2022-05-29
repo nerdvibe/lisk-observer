22ea06601dacc255f17c2120876e6bc412a3c7c4
Cryptosaka <79313706+Cryptosaka@users.noreply.github.com>
2022-05-30 00:47:13 +0200

    Added username in url recognition

--- lisk-observer-client/src/modules/account/accountProfile/AccountContainer.tsx
+++ lisk-observer-client/src/modules/account/accountProfile/AccountContainer.tsx
@@ -3,6 +3,11 @@ import { useParams } from "react-router-dom";
 import { CurrentAccount } from "./currentAccount/CurrentAccount";
 import { LegacyAccount } from "./legacyAccount/LegacyAccount";
 import { isLegacyAddress } from "../utils/account";
+import { IsErrorOrLoading } from "../../utils/IsErrorOrLoading";
+import { isAddress } from "../../utils/lisk/utils/lisk/regex";
+import { useLazyQuery } from "@apollo/client";
+import { useEffect } from "react";
+import { gql } from "graphql.macro";
 
 export interface AccountContainerParams {
   addressContext: string;
@@ -10,11 +15,56 @@ export interface AccountContainerParams {
   tab?: string;
 }
 
+const USERNAME_QUERY = gql`
+  query {
+    lastBlocks {
+      data {
+        id
+        height
+        timestamp
+        generatorPublicKey
+        finalized
+        isFinal
+        address
+        username
+        reward
+      }
+    }
+  }
+`; // QUERY PLACEHOLDER
+
 export const AccountContainer: React.FC = () => {
   let { addressContext: addressContextParam } = useParams<
     AccountContainerParams
   >();
+  const [
+    getUsernameData,
+    { data: usernameData, loading: usernameLoading, error: usernameError },
+  ] = useLazyQuery(USERNAME_QUERY);
   const isLegacy = isLegacyAddress(addressContextParam);
+  const isUsername = !(isAddress(addressContextParam) || isLegacy);
+
+  useEffect(() => {
+    if (isUsername) {
+      getUsernameData({
+        variables: {
+          username: addressContextParam,
+        },
+      });
+    }
+  }, []);
+
+  if (isUsername && (usernameLoading || usernameError)) {
+    return (
+      <div className="content">
+        <IsErrorOrLoading
+          error={!!usernameError}
+          errorMsg={`Username ${addressContextParam} not found`}
+          title={"Account page"}
+        />
+      </div>
+    );
+  }
 
   return (
     <div className="content">
--- lisk-observer-client/src/modules/utils/lisk/utils/lisk/regex.ts
+++ lisk-observer-client/src/modules/utils/lisk/utils/lisk/regex.ts
@@ -1,4 +1,4 @@
-export const addressRegex = /^lsk[a-z0-9!@$&_.]{0,20}$/;
+export const addressRegex = /^lsk[a-z0-9!@$&_.]{0,40}$/;
 export const delegateNameRegex = /^[a-z0-9!@$&_.]{0,20}$/;
 export const transactionIdRegex = /^[1-9]\d{0,19}$/;
 export const blockIdRegex = /^[1-9]\d{0,19}$/;
